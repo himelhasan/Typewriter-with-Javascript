1287a9da70fcf95380544d03a78d49f40bd6f3ac
Himel Hasan <himelhasan497@gmail.com>
2022-09-15 00:30:52 +0600

    fixed  the countdown second, now shoing only digit without decimal values

--- script.js
+++ script.js
@@ -139,5 +139,7 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${
+    parseInt(startTime) ? parseInt(timeSpent) : 0
+  } seconds`;
 }, 1000);
